df0bf8b1625ad2dd9e56c19071ec2e5a81005d98
associacioadecte <associacioadecte@gmail.com>
2026-09-25 22:25:10 +0200

    Jornada Espacial

--- assets/data/navegacio.js
+++ assets/data/navegacio.js
@@ -25,7 +25,8 @@ window.ADECTE_NAV = [
     submenu: [
       { text: 'Totes les activitats', pagina: 'activitats', href: 'activitats.html' },
       {text: 'Presentació ADECTE', pagina: 'presentacio', href: 'presentacio-adecte.html'},
-      { text: 'Eclipsi Solar 2026', pagina: 'eclipsi', href: 'eclipsi.html' }
+      { text: 'Eclipsi Solar 2026', pagina: 'eclipsi', href: 'eclipsi.html' },
+      { text: 'I Jornada Espacial', pagina: 'jornada-espacial', href: 'jornada-espacial.html' }
     ]
   },
   {
